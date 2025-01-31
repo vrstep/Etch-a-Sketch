eea7055e48b576665e143da2d36997b0fe0a5a29
Amir <amir.batyrggaliev@gmail.com>
2025-01-31 20:49:24 +0500

    Create the grid canvas 16x16

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const mainContainer = document.querySelector("#main-container");
 
 function createGrid() {
-    const grid = document.createElement("div");
-    grid.className = "grid";
-    mainContainer.append(grid);
+    for (let i = 0; i < 16; i++) {
+        const grid = document.createElement("div");
+        grid.className = "grid";
+        mainContainer.append(grid);
+        for (let j = 0; j < 15; j++) {
+            const grid = document.createElement("div");
+            grid.className = "grid";
+            mainContainer.append(grid);
+        }
+    }
 }
 
 createGrid();
