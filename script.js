b2dae6e9148e82db35e1502a09108919bbc1d61d
Amir <amir.batyrggaliev@gmail.com>
2025-01-31 20:59:59 +0500

    Create grid container, nest it in main container

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const mainContainer = document.querySelector("#main-container");
+const gridContainer = document.querySelector("#grid-container");
 
 function createGrid() {
     for (let i = 0; i < 16; i++) {
         const grid = document.createElement("div");
         grid.className = "grid";
-        mainContainer.append(grid);
+        gridContainer.append(grid);
         for (let j = 0; j < 15; j++) {
             const grid = document.createElement("div");
             grid.className = "grid";
-            mainContainer.append(grid);
+            gridContainer.append(grid);
         }
     }
 }
