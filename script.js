47043154614f57f52e54b14bf6ca2f64bbcb7e5e
Amir <amir.batyrggaliev@gmail.com>
2025-02-01 15:37:32 +0500

    Implement function that prompts user for grid size

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const gridContainer = document.querySelector("#grid-container");
+const buttonInputGridSize = document.querySelector("#btn-prompt-grid-size");
+
+// To find width formula grid 18x18, multiply grid widthxheight * grid size; e.g 18x24= 432px
+
+const gridSize = promptGridSize();
 
 function createGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const grid = document.createElement("div");
         grid.className = "grid";
         gridContainer.append(grid);
-        for (let j = 0; j < 15; j++) {
-            const grid = document.createElement("div");
-            grid.className = "grid";
-            gridContainer.append(grid);
-        }
+        // for (let j = 0; j < 15; j++) {
+        //     const grid = document.createElement("div");
+        //     grid.className = "grid";
+        //     gridContainer.append(grid);
+        // }
     }
+}
+
+function promptGridSize() {
+    const gridSize = prompt("What size of grid you want?", "");
+    return gridSize;
 }
 
 function hoverGrid() {
